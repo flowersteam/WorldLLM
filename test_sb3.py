197dc9a8f0d9482d8fc3643c08008eb06c7515ff
Liphos <levyguillaume10@yahoo.fr>
2024-10-07 21:45:50 +0200

    feat: replace count based by rewards by transition index
    feat: Remove CB and modify reward between end of rollout and training
    
    feat: Add Corrected reward to plot
    
    Plot mean reward across all envs and nsteps
    
    feat: change reward for test to alp between RB and DB
    
    fix: seed based on seed instead of nn size

--- test_sb3.py
+++ test_sb3.py
@@ -2,25 +2,246 @@ import argparse
 import random
 from functools import partial
 from itertools import product
-from typing import Any, Dict
+from typing import Any, ClassVar, Dict, Optional, Tuple, Type, TypeVar, Union
 
 import gymnasium
 import numpy as np
-import torch
+import torch as th
+from gymnasium import spaces
 from sb3_contrib import MaskablePPO
-
-# This is a drop-in replacement for EvalCallback
-from sb3_contrib.common.maskable.callbacks import MaskableEvalCallback
-from sb3_contrib.common.maskable.evaluation import evaluate_policy
-from sb3_contrib.common.maskable.utils import get_action_masks
+from sb3_contrib.common.maskable.buffers import (
+    MaskableRolloutBuffer,
+    MaskableRolloutBufferSamples,
+)
+from sb3_contrib.common.maskable.utils import get_action_masks, is_masking_supported
 from sb3_contrib.common.wrappers import ActionMasker
+from stable_baselines3.common.buffers import DictRolloutBuffer, RolloutBuffer
 from stable_baselines3.common.callbacks import BaseCallback
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.logger import TensorBoardOutputFormat
-from stable_baselines3.common.monitor import Monitor
-from stable_baselines3.common.preprocessing import get_flattened_obs_dim
+from stable_baselines3.common.utils import obs_as_tensor
+from stable_baselines3.common.vec_env import VecEnv
+from torch.nn import functional as F
 
 from worldllm_envs.base import BaseRuleEnv
+
+
+def correct_reward(prev_rewards: np.ndarray):
+    """Compute the rewards given the previous rewards. To gain time the previous rewards are indices for the new rewards"""
+    lst_rewards = np.array(
+        [
+            0.1,
+            0.09,
+            0.21,
+            0.03,
+            0.77,
+            0.87,
+            1.74,
+        ]
+    )
+    new_rewards = lst_rewards[prev_rewards.round().astype(int)]
+    return new_rewards
+
+
+class CustomMaskableRolloutBuffer(MaskableRolloutBuffer):
+    """
+    Modification of the rollout buffer to include true rewards tracking
+    """
+
+    true_rewards: np.ndarray
+
+    def reset(self) -> None:
+        self.true_rewards = np.zeros((self.buffer_size, self.n_envs), dtype=np.float32)
+        super().reset()
+
+    def add(
+        self,
+        *args,
+        action_masks: Optional[np.ndarray] = None,
+        true_rewards: Optional[np.ndarray] = None,
+        **kwargs,
+    ) -> None:
+        """
+        :param action_masks: Masks applied to constrain the choice of possible actions.
+        """
+        self.true_rewards[self.pos] = np.array(true_rewards)
+        if action_masks is not None:
+            self.action_masks[self.pos] = action_masks.reshape(
+                (self.n_envs, self.mask_dims)
+            )
+
+        super().add(*args, **kwargs)
+
+
+class CustomMaskablePPO(MaskablePPO):
+    """Modification of the PPO algorithm to modify rewards between end of the collecting of the rollout
+    and the training"""
+
+    def collect_rollouts(
+        self,
+        env: VecEnv,
+        callback: BaseCallback,
+        rollout_buffer: RolloutBuffer,
+        n_rollout_steps: int,
+        use_masking: bool = True,
+    ) -> bool:
+        """
+        Collect experiences using the current policy and fill a ``RolloutBuffer``.
+        The term rollout here refers to the model-free notion and should not
+        be used with the concept of rollout used in model-based RL or planning.
+
+        This method is largely identical to the implementation found in the parent class.
+
+        :param env: The training environment
+        :param callback: Callback that will be called at each step
+            (and at the beginning and end of the rollout)
+        :param rollout_buffer: Buffer to fill with rollouts
+        :param n_steps: Number of experiences to collect per environment
+        :param use_masking: Whether or not to use invalid action masks during training
+        :return: True if function returned with at least `n_rollout_steps`
+            collected, False if callback terminated rollout prematurely.
+        """
+
+        assert isinstance(
+            rollout_buffer, CustomMaskableRolloutBuffer
+        ), "RolloutBuffer doesn't support action masking"
+        assert self._last_obs is not None, "No previous observation was provided"
+        # Switch to eval mode (this affects batch norm / dropout)
+        self.policy.set_training_mode(False)
+        n_steps = 0
+        action_masks = None
+        rollout_buffer.reset()
+
+        if use_masking and not is_masking_supported(env):
+            raise ValueError(
+                "Environment does not support action masking. Consider using ActionMasker wrapper"
+            )
+
+        callback.on_rollout_start()
+
+        while n_steps < n_rollout_steps:
+            with th.no_grad():
+                # Convert to pytorch tensor or to TensorDict
+                obs_tensor = obs_as_tensor(self._last_obs, self.device)
+
+                # This is the only change related to invalid action masking
+                if use_masking:
+                    action_masks = get_action_masks(env)
+
+                actions, values, log_probs = self.policy(
+                    obs_tensor, action_masks=action_masks
+                )
+
+            actions = actions.cpu().numpy()
+            new_obs, rewards, dones, infos = env.step(actions)
+            # Copy rewards to get reward before adding the value function
+            true_rewards = np.copy(rewards)
+
+            self.num_timesteps += env.num_envs
+
+            # Give access to local variables
+            callback.update_locals(locals())
+            if not callback.on_step():
+                return False
+
+            self._update_info_buffer(infos, dones)
+            n_steps += 1
+
+            if isinstance(self.action_space, spaces.Discrete):
+                # Reshape in case of discrete action
+                actions = actions.reshape(-1, 1)
+
+            # Handle timeout by bootstraping with value function
+            # see GitHub issue #633
+            for idx, done in enumerate(dones):
+                if (
+                    done
+                    and infos[idx].get("terminal_observation") is not None
+                    and infos[idx].get("TimeLimit.truncated", False)
+                ):
+                    terminal_obs = self.policy.obs_to_tensor(
+                        infos[idx]["terminal_observation"]
+                    )[0]
+                    with th.no_grad():
+                        terminal_value = self.policy.predict_values(terminal_obs)[0]
+                    rewards[idx] += self.gamma * terminal_value
+
+            rollout_buffer.add(
+                self._last_obs,
+                actions,
+                rewards,
+                self._last_episode_starts,
+                values,
+                log_probs,
+                action_masks=action_masks,
+                true_rewards=true_rewards,
+            )
+            self._last_obs = new_obs  # type: ignore[assignment]
+            self._last_episode_starts = dones
+
+        with th.no_grad():
+            # Compute value for the last timestep
+            # Masking is not needed here, the choice of action doesn't matter.
+            # We only want the value of the current observation.
+            values = self.policy.predict_values(obs_as_tensor(new_obs, self.device))  # type: ignore[arg-type]
+
+        # We need to adjust the reward
+        correct_rewards = correct_reward(self.rollout_buffer.true_rewards)
+        # We need to keep the terminated value in case. The true rewards are there for that
+        self.rollout_buffer.rewards = (
+            self.rollout_buffer.rewards - self.rollout_buffer.true_rewards
+        ) + correct_rewards
+        # End
+        rollout_buffer.compute_returns_and_advantage(last_values=values, dones=dones)
+
+        callback.update_locals(locals())
+        callback.on_rollout_end()
+
+        return True
+
+    def learn(  # type: ignore[override]
+        self,
+        total_timesteps: int,
+        callback=None,
+        log_interval: int = 1,
+        tb_log_name: str = "PPO",
+        reset_num_timesteps: bool = True,
+        use_masking: bool = True,
+        progress_bar: bool = False,
+    ):
+        iteration = 0
+
+        total_timesteps, callback = self._setup_learn(
+            total_timesteps,
+            callback,
+            reset_num_timesteps,
+            tb_log_name,
+            progress_bar,
+        )
+
+        callback.on_training_start(locals(), globals())
+
+        assert self.env is not None
+
+        while self.num_timesteps < total_timesteps:
+            continue_training = self.collect_rollouts(
+                self.env, callback, self.rollout_buffer, self.n_steps, use_masking
+            )
+
+            if not continue_training:
+                break
+
+            iteration += 1
+            self._update_current_progress_remaining(self.num_timesteps, total_timesteps)
+
+            # Display training infos
+            if log_interval is not None and iteration % log_interval == 0:
+                self._dump_logs(iteration)
+
+            self.train()
+
+        callback.on_training_end()
+
+        return self
 
 
 class TransitionCounterCallback(BaseCallback):
@@ -32,6 +253,12 @@ class TransitionCounterCallback(BaseCallback):
         self.all_timestamps_sh = []
         self.all_timestamps_p = []
         self.hyperparameters = hyperparameters
+
+    def _on_rollout_end(self) -> None:
+        """Save mean of the true reward"""
+        self.logger.record(
+            "rollout/Corrected_mean_reward", np.mean(self.locals.get("correct_rewards"))
+        )
 
     def _on_step(self) -> bool:
         # Access the current observation and info
@@ -91,7 +318,7 @@ def mask_fn(env):
     return env.unwrapped.action_mask
 
 
-def make_env(countbased_dict: Dict[str, int]):
+def make_env():
     env: BaseRuleEnv = gymnasium.make(
         "worldllm_envs/Playground-v1",
         **{
@@ -102,7 +329,6 @@ def make_env(countbased_dict: Dict[str, int]):
     new_rule = "grow any small_herbivorous then grow any big_herbivorous"
     env.reset(options={"rule": new_rule})
     env = ActionMasker(env, mask_fn)  # Wrap to enable masking
-    env.unwrapped.set_shared_countbased(countbased_dict)
     return env
 
 
@@ -120,30 +346,21 @@ if __name__ == "__main__":
 
     # Set the seed
     np.random.seed(config["seed"])
-    torch.manual_seed(config["nn_size"])
-    torch.cuda.manual_seed_all(config["nn_size"])
+    th.manual_seed(config["seed"])
+    th.cuda.manual_seed_all(config["seed"])
     random.seed(config["seed"])
     n_envs = 9
 
-    # Load first environment
-    env: BaseRuleEnv = gymnasium.make(
-        "worldllm_envs/Playground-v1",
-        **{"max_steps": 30, "playground_config": {"max_nb_objects": 8}},
-    )
-    new_rule = "grow any small_herbivorous then grow any big_herbivorous"
-    env.reset(options={"rule": new_rule})
-    env = ActionMasker(env, mask_fn)  # Wrap to enable masking
-    countbased = env.unwrapped.get_shared_countbased()
-
     envs = make_vec_env(
-        partial(make_env, countbased_dict=countbased),
+        partial(make_env),
         n_envs=n_envs,
     )
     tensorboard_log_name = f"./PPO_{config['seed']}"
     # Train PPO
-    model = MaskablePPO(
+    model = CustomMaskablePPO(
         "MlpPolicy",
         envs,
+        rollout_buffer_class=CustomMaskableRolloutBuffer,
         gamma=config["gamma"],
         learning_rate=config["lr"],
         ent_coef=0.01,
@@ -153,7 +370,7 @@ if __name__ == "__main__":
         gae_lambda=config["gae"],
         device="cuda",
         verbose=1,
-        tensorboard_log="./logs_ppo_sb3_cb",
+        tensorboard_log="./logs_ppo_sb3_test",
         policy_kwargs=dict(
             net_arch=dict(
                 pi=[config["nn_size"], config["nn_size"]],
@@ -173,8 +390,9 @@ if __name__ == "__main__":
     )
     model.save("ppo_mask")
     # Load model
-    model = MaskablePPO.load("ppo_mask")
+    model = CustomMaskablePPO.load("ppo_mask")
 
+    env = make_env()
     for _ in range(3):
         print("New episode")
         obs, _ = env.reset()
--- worldllm_envs/worldllm_envs/playground/playground_text_wrapper.py
+++ worldllm_envs/worldllm_envs/playground/playground_text_wrapper.py
@@ -1216,6 +1216,16 @@ def generate_diverse_trajectories(
 
 
 class PlayGroundDiscrete(PlayGroundText):
+    TRANSITION_TYPE_TO_ID = {
+        "nothing": 0,
+        "standing": 1,
+        "holding1": 2,
+        "holding2": 3,
+        "transformP": 4,
+        "transformSH": 5,
+        "transformBH": 6,
+    }
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         # 4 because 1 for what you see, 1 for what you stand on, 2 for what you hold
@@ -1260,19 +1270,6 @@ class PlayGroundDiscrete(PlayGroundText):
 
         # WE save last observation to compute the difference
         self._last_text_obs = None
-
-        # Keep count of the transitions:
-
-        # Keep count of the transtitions:
-        self.count_based = {
-            "nothing": 0,
-            "standing": 0,
-            "holding1": 0,
-            "holding2": 0,
-            "transformP": 0,
-            "transformSH": 0,
-            "transformBH": 0,
-        }
 
     def obj_to_index(self, incr: int, obj_name: str) -> Tuple[int, int, int, int]:
         """Return the index of the object in the observation"""
@@ -1419,7 +1416,7 @@ class PlayGroundDiscrete(PlayGroundText):
 
     def get_diff(
         self, last_observation: str, observation: str, action: str
-    ) -> Tuple[str, float, str]:
+    ) -> Tuple[str, str]:
         """Compute the difference between two observations and return reward"""
         # Split text description
         last_obs_obj, last_obs_stand, last_obs_hold = self._split_description(
@@ -1427,38 +1424,23 @@ class PlayGroundDiscrete(PlayGroundText):
         )
         obs_obj, obs_stand, obs_hold = self._split_description(observation)
         action_type, action_obj = self._split_action(action)
-        rewards = {
-            "nothing": 1.8,
-            "standing": 1.2,
-            "holding1": 0.5,
-            "holding2": 4.2,
-            "transformP": 5.0,
-            "transformSH": 3.8,
-            "transformBH": 11.0,
-        }
-        count_based_func = lambda x: rewards[x] / (self.count_based[x] ** 0.4)
         if (
             Counter(last_obs_obj) == Counter(obs_obj)
             and Counter(last_obs_stand) == Counter(obs_stand)
             and Counter(last_obs_hold) == Counter(obs_hold)
         ):
-            self.count_based["nothing"] += 1
             return (
                 "Nothing has changed.",
-                count_based_func("nothing"),
                 "nothing",
             )
         elif action_type == "go to":
-            self.count_based["standing"] += 1
             if action_obj == "nothing":
                 return (
                     "You are standing on nothing.",
-                    count_based_func("standing"),
                     "standing",
                 )
             return (
                 f"You are standing on the {action_obj}.",
-                count_based_func("standing"),
                 "standing",
             )
         elif action_type == "grasp":
@@ -1467,17 +1449,13 @@ class PlayGroundDiscrete(PlayGroundText):
                 len(counter_diff) == 1
             ), "There should be only one object grasped at a time"
             if last_obs_hold[0] == "empty":
-                self.count_based["holding1"] += 1
                 return (
                     f"You are holding the {list(counter_diff.keys())[0]}.",
-                    count_based_func("holding1"),
                     "holding1",
                 )
             if len(last_obs_hold) == 1:
-                self.count_based["holding2"] += 1
                 return (
                     f"You are holding the {last_obs_hold[0]} and the {list(counter_diff.keys())[0]}.",
-                    count_based_func("holding2"),
                     "holding2",
                 )
             raise ValueError("Inventory cannot contain more than 2 objects")
@@ -1488,16 +1466,10 @@ class PlayGroundDiscrete(PlayGroundText):
             new_object_type = list(new_obj.keys())[0]
             new_object_category = self.types_to_categories[new_object_type]
             if new_object_category == "plant":
-                self.count_based["transformP"] += 1
-                reward = count_based_func("transformP")
                 transition_type = "transformP"
             elif new_object_category == "small_herbivorous":
-                self.count_based["transformSH"] += 1
-                reward = count_based_func("transformSH")
                 transition_type = "transformSH"
             elif new_object_category == "big_herbivorous":
-                self.count_based["transformBH"] += 1
-                reward = count_based_func("transformBH")
                 transition_type = "transformBH"
             else:
                 raise ValueError(
@@ -1506,12 +1478,10 @@ class PlayGroundDiscrete(PlayGroundText):
             if action_obj == "all":
                 return (
                     f"The {last_obs_hold[0]}, {last_obs_hold[1]} and {list(old_obj)[0]} transform into the {list(new_obj)[0]}.",
-                    reward,
                     transition_type,
                 )
             return (
                 f"The {action_obj} and {list(old_obj)[0]} transform into the {list(new_obj)[0]}.",
-                reward,
                 transition_type,
             )
         raise ValueError(
@@ -1581,7 +1551,7 @@ class PlayGroundDiscrete(PlayGroundText):
         self._update_action_mask(obs)
         # Compute the str description
         obs_desc, info_description = self.generate_description()
-        text_obs, reward, transition_type = self.get_diff(
+        text_obs, transition_type = self.get_diff(
             self._last_text_obs, obs_desc, action_str
         )
         text_action = self.action_to_text(action_str)
@@ -1600,16 +1570,12 @@ class PlayGroundDiscrete(PlayGroundText):
         # Reset the size of obj help to find the obj grown in the current step
         self.playground.unwrapped.reset_size()
 
-        return obs.flatten(), reward, done, False, info
+        # For alp, the reward is the index on the transition type
+
+        return obs.flatten(), self.TRANSITION_TYPE_TO_ID[transition_type], done, False, info
 
     def render(self):
         raise NotImplementedError("Rendering is not supported for the environment")
 
     def get_action_mask(self) -> np.ndarray:
         return self.action_mask
-
-    def get_shared_countbased(self) -> Dict[str, int]:
-        return self.count_based
-
-    def set_shared_countbased(self, count_based: Dict[str, int]) -> None:
-        self.count_based = count_based
