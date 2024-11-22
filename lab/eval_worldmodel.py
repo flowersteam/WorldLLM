b56093167ee5410d328eaa2570bdc145313968d6
Liphos <levyguillaume10@yahoo.fr>
2024-11-22 20:13:52 +0100

    feat: add script to evaluate worldmodel prediction in constrained decoding

---
+++ lab/eval_worldmodel.py
@@ -0,0 +1,101 @@
+import os
+import sys
+
+import gymnasium
+from tqdm import tqdm
+
+from worldllm_envs.base import BaseRuleEnv, RandomAgent, Trajectory
+from worldllm_envs.playground.playground_text_wrapper import PerfectAgent
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from utils.utils_llm import (
+    Statistician,
+    build_stat_prompt_info,
+    compute_likelihood,
+    load_transformers,
+)
+
+SEED = 15
+NB_EPISODES = 3
+
+# Load the environment
+env: BaseRuleEnv = gymnasium.make(
+    "worldllm_envs/PlaygroundText-v1",
+    **{"seed": SEED, "test_dataset_path": None, "max_steps": 30},
+)
+new_rule = env.generate_rule("Grow any small_herbivorous then grow any big_herbivorous")
+env.reset(options={"rule": new_rule})
+
+# Load the LLM
+config = {
+    "name": "microsoft/Phi-3-mini-4k-instruct",
+    "model_params": {},
+    "tokenizer_params": {},
+    "is_quantized": True,
+    "generation_kwargs": {"cache_implementation": None, "cache_config": None},
+    "chat_template": "{{ bos_token }}{% for message in messages %}{% if (message['role'] == 'system') %}{{'<|system|>' + '\n' + message['content'] + '<|end|>' + '\n'}}{% elif (message['role'] == 'user') %}{{'<|user|>' + '\n' + message['content'] + '<|end|>' + '\n' + '<|assistant|>' + '\n'}}{% elif message['role'] == 'assistant' %}{{message['content'] + '<|end|>' + '\n'}}{% endif %}{% endfor %}",
+}
+world_model = load_transformers(config)
+stat_prompt_info = build_stat_prompt_info(
+    world_model,
+    env.get_message_info(),
+    "You like doing a lot of puzzles. Please answer with a brief answer and be as precise as you can.",
+    10,
+)
+statistician = Statistician(
+    world_model[0],
+    world_model[1],
+    stat_prompt_info,
+)
+rules_to_test = [None]
+# Create the different agents
+
+perfect_agent_sh = PerfectAgent(
+    env.action_space, curriculum_goals=["Grow any small_herbivorous"]
+)
+perfect_agent_shbh = PerfectAgent(
+    env.action_space,
+    curriculum_goals=["Grow any small_herbivorous then grow any big_herbivorous"],
+)
+random_agent = RandomAgent(env.action_space)
+all_scores = []
+pbar = tqdm(range(3 * NB_EPISODES), desc="Generating trajectories")
+for agent in [perfect_agent_sh, perfect_agent_shbh, random_agent]:
+    for _ in range(NB_EPISODES):
+        all_scores_per_episode = []
+        obs, info = env.reset()
+        reset_info = {"pipeline_progression": 0}
+        info.update(reset_info)
+        # Compute plan
+        agent.reset(info)
+        done = False
+        while not done:
+            # Record inputs from keyboard
+            # Print possible actions
+            action, agent_done = agent(obs, **info)
+            all_transitions = env.get_all_possible_transitions()
+
+            # Get the score of the LLM for all transitions
+            lst_trajectories = [
+                Trajectory(
+                    lst_obs=[
+                        info["trajectory_obs_text"][-1],
+                        "",
+                    ],  # We don't use the last observation
+                    lst_act=[env.action_to_text(action)],
+                    lst_diff=[transition],
+                )
+                for transition in all_transitions
+            ]
+            (_, logp), _ = compute_likelihood(
+                statistician, rules_to_test, lst_trajectories, return_all_logp=True
+            )
+            all_scores_per_episode.append(logp)
+            obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated or agent_done
+        pbar.update(1)
+        all_scores.append(all_scores_per_episode)
+
+
+print("Done.")
